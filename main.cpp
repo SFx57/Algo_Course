4b2327be43d493148e0335ab226fc80a1eb6ed43
Rodion <109344530+SFx57@users.noreply.github.com>
2022-11-24 13:11:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
 // 70437052
 #include <iostream>
 
+int Count(int first, int now) { return (now > first ? 1 : 0); }
+
 int main() {
-  int n;
+  size_t n;
   std::cin >> n;
   int num_first;
   int num_now;
   int k = 1;
   std::cin >> num_first;
-  for (int i = 0; i != n - 1; ++i) {
+
+  for (size_t i = 0; i != n - 1; ++i) {
     std::cin >> num_now;
-    (num_now > num_first) ? ++k : k;
+    k += Count(num_first, num_now);
   }
+
   std::cout << k % n;
 }
